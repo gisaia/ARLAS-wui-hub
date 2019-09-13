0f6a19ed48a7bc428c501dddc640b4867c3a2b42
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2019-09-13 11:47:06 +0200

    Improve documentation with Docker section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,13 @@ export class AppComponent {
   public cards: any;
   public header: any;
   public footer: any;
+  public items: any;
 
   constructor(private loadService: LoadService){
     this.header = this.loadService.appData.header;
     this.footer = this.loadService.appData.footer;
     this.cards = this.loadService.appData.cards;
+    this.items = this.footer.items;
   }
 
   public navigate(url: string) {
