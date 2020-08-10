9e09415af9fa27a734e5be85b092efbad2d25bb0
MohamedHamou <mohamed.hamou@gisaia.com>
2020-08-10 11:27:03 +0200

    Inject startup service

--- src/app/services/load.service.ts
+++ src/app/services/load.service.ts
@@ -1,31 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ArlasCollaborativesearchService, ArlasConfigService, 
-  ArlasExploreApi, ArlasStartupService, ArlasSettings } from 'arlas-wui-toolkit';
-import { ArlasConfigurationUpdaterService } from 'arlas-wui-toolkit/services/configuration-updater/configurationUpdater.service';
-import { FETCH_OPTIONS, CONFIG_UPDATER } from 'arlas-wui-toolkit/services/startup/startup.service';
-import { PersistenceService } from 'arlas-wui-toolkit/services/persistence/persistence.service';
-import { TranslateService } from '@ngx-translate/core';
+import { ArlasStartupService, ArlasSettings } from 'arlas-wui-toolkit';
 
 @Injectable()
-export class LoadService extends ArlasStartupService {
+export class LoadService {
 
   public appData: any;
 
   constructor(
-      configService: ArlasConfigService,
-      arlasCss: ArlasCollaborativesearchService,
-      configurationUpdaterService: ArlasConfigurationUpdaterService,
-      injector: Injector,
-    @Inject(FETCH_OPTIONS)   fetchOptions,
-     http: HttpClient,
-     private https: HttpClient,
+      private arlasStartupService: ArlasStartupService,
+      private https: HttpClient) {
 
-     translateService: TranslateService,
-    @Inject(CONFIG_UPDATER)  configUpdater,
-     persistenceService: PersistenceService) { 
-      super(configService, arlasCss, configurationUpdaterService, injector,
-        fetchOptions, http, translateService, configUpdater, persistenceService)
     }
 
   public load(config: string): Promise<any> {
@@ -33,9 +18,9 @@ export class LoadService extends ArlasStartupService {
       .get(config)
       .toPromise()
       .then(response => this.appData = response)
-      .then(()=> this.applyAppSettings())
-      .then((s: ArlasSettings) => this.authenticate(s))
-      .then((s: ArlasSettings) => this.enrichHeaders(s))
+      .then(() => this.arlasStartupService.applyAppSettings())
+      .then((s: ArlasSettings) => this.arlasStartupService.authenticate(s))
+      .then((s: ArlasSettings) => this.arlasStartupService.enrichHeaders(s))
       .catch(error => {
         console.log(error);
       });
