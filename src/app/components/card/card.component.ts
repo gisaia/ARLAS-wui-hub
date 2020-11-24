3a07ddec0a392102f2b7f533c93a8dd1d668a35f
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2020-11-24 12:25:30 +0100

    Add informations on cards
    Add status, collection
    Allow to filter card on collection
    
    Fix #41
    Fix #42
    Fix #45
    Fix #46
    Fix #50

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,42 +1,60 @@
-import { Component, Input, Output, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, Input, Output, AfterViewInit, ViewChild, OnInit } from '@angular/core';
+import { ArlasColorGeneratorLoader } from 'arlas-wui-toolkit';
 import { Subject } from 'rxjs';
 import { Card } from '../../services/card.service';
 
 
 export enum Action {
-  VIEW,
-  DELETE,
-  EDIT,
-  DUPLICATE,
-  SHARE
+    VIEW,
+    DELETE,
+    EDIT,
+    DUPLICATE,
+    SHARE
 }
 
 export interface CardAction {
-  card: Card;
-  action: Action;
+    card: Card;
+    action: Action;
 }
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements AfterViewInit {
+export class CardComponent implements AfterViewInit, OnInit {
 
-  @Input() public card: Card;
-  @Output() public actionOnCard: Subject<CardAction> = new Subject<CardAction>();
 
-  public action = Action;
-  constructor() {
-  }
+    @Input() public card: Card;
+    @Output() public actionOnCard: Subject<CardAction> = new Subject<CardAction>();
 
-  public ngAfterViewInit() {
-    const c = Object.assign(this.card.actions);
-    this.card.actions = new Array();
-    this.card.actions = c;
-  }
+    public action = Action;
+    public collectionColor = '#000000';
+    public status = 'private';
 
-  public afterAction() {
-    this.actionOnCard.next();
-  }
+    public readers: string[] = [];
+    public writers: string[] = [];
+
+    constructor(
+        private arlasColorGeneratorLoader: ArlasColorGeneratorLoader
+    ) {
+    }
+    public ngOnInit(): void {
+        this.readers = this.card.readers.map(g => g.name);
+        this.writers = this.card.writers.map(g => g.name);
+
+        this.status = ((this.readers.length === 0 && this.writers.length === 0) || this.card.owner === 'anonymous') ? 'private' :
+            (this.readers.includes('public') || this.writers.includes('public')) ? 'public' : 'shared';
+        this.collectionColor = this.arlasColorGeneratorLoader.getColor(this.card.collection);
+    }
+
+    public ngAfterViewInit() {
+        const c = Object.assign(this.card.actions);
+        this.card.actions = new Array();
+        this.card.actions = c;
+    }
+
+    public afterAction() {
+        this.actionOnCard.next();
+    }
 }
