4f3fb16f798f315213d061c83bb52515fed3b36f
mohamed.hamou.gisaia <mohamed.hamou@gisaia.com>
2025-09-08 14:53:03 +0200

    Bump to toolkit 27.1.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { LoadService } from './services/load.service';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ArlasToolKitModule, ArlasToolkitSharedModule } from 'arlas-wui-toolkit';
 import { SidenavService } from './services/sidenav.service';
 describe('AppComponent', () => {
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent
